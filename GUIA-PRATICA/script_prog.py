361c348e7c6a3bcde3233d7d75c4f006376de1f3
nickyzinn16 <rickydavidbans27@gmail.com>
2026-04-20 17:31:10 -0100

    Adicionar projeto completo

---
+++ GUIA-PRATICA/script_prog.py
@@ -0,0 +1,68 @@
+from abc import ABC, abstractmethod
+
+class Pessoa(ABC):
+    def __init__(self, name=None, email=None, id=None):
+        self.name = name
+        self.email = email
+        self.id = id
+
+    @abstractmethod
+    def get_type(self):
+        pass
+
+    @abstractmethod
+    def get_description(self):
+        pass
+
+    def __str__(self):
+        return f"{self.name} ({self.get_type()})"
+
+
+class Aluno(Pessoa):
+    def __init__(self, name=None, email=None, id=None, notas=None):
+        super().__init__(name, email, id)
+        self.notas = notas
+
+    def calcular_media(self):
+        return sum(self.notas) / len(self.notas)
+
+    def get_type(self):
+        return "Aluno da escola"
+
+    def get_description(self):
+        return f"O aluno {self.name}, tem média de {self.calcular_media():.2f}"
+
+
+class Pussor(Pessoa):
+    def __init__(self, name=None, email=None, id=None, subject=None):
+        super().__init__(name, email, id)
+        self.subject = subject
+
+    def get_subjcts(self):
+        return self.subject
+
+    def get_type(self):
+        return "Professor da escola"
+
+    def get_description(self):
+        return f"O professor {self.name}, leciona {self.subject}."
+
+if __name__ == "__main__":
+    # Processamento do Aluno
+    print("ok")
+    aluno1 = Aluno("Mario Santos", "msantos@gmail.com", 12, [15, 10, 13])
+    aluno2 = Aluno("Kylian Mbappe", "kmbappe@gmail.com", 18, [10, 10, 20])
+    aluno3 = Aluno("Lamine Yamal", "lyamal@gmail.com", 10, [15, 10, 12])
+    prof1 = Pussor("Pep Guardiola", "guardola@gmail.com", 10, "Tiki taka")
+    prof2 = Pussor("Jose Mourinho", "mourinho@gmail.com", 10, "Retranca Total")
+
+    pessoas = [aluno1, aluno2, aluno3, prof1, prof2]
+
+    for pessoa in pessoas:
+        print(pessoa)
+
+    for pessoa in pessoas:
+        print(pessoa.get_description())
+
+    
+    # Processamento do Professor TRAZER INTERACAO COM BASE DE DADOS ATRAVES DE HERANCA E POLIMOFIRMOgit clone https://github.com/nickyzinn16/GUIA-PRATICA.git
